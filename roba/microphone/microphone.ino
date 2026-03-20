803d4cf9ccf5ccaf46e47f6966f5a35be16e20b2
Vic <victor.dastru@gmail.com>
2026-03-20 18:18:02 +0100

    Println di debug

--- roba/microphone/microphone.ino
+++ roba/microphone/microphone.ino
@@ -11,17 +11,20 @@ void setup() {
     Serial.println("Failed to start PDM!");
     while(1);
   }
+  Serial.println("Starting");
 }
 
 void loop() {
   if (samplesRead) {
+    Serial.println("before noInterrupts()");
     noInterrupts();
-    Serial.println("noInterrupts()");
+    Serial.println("after noInterrupts()");
     for (int i = 0; i < samplesRead; i++) {
       Serial.println(sampleBuffer[i]);
     }
+    Serial.println("before interrupts()");
     interrupts();
-    Serial.println("noInterrupts()");
+    Serial.println("after interrupts()");
     samplesRead = 0;
   }
 }
