4c165fe94e23010718413fab4755ffd91a1302a9
Vic <victor.dastru@gmail.com>
2026-03-20 18:08:01 +0100

    Esempio microfono (non so se funziona)

---
+++ roba/microphone/microphone.ino
@@ -0,0 +1,33 @@
+#include <PDM.h>
+
+short sampleBuffer[256];
+volatile int samplesRead = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  PDM.onReceive(onPDMdata);
+  if (!PDM.begin(1, 16000)) {
+    Serial.println("Failed to start PDM!");
+    while(1);
+  }
+}
+
+void loop() {
+  if (samplesRead) {
+    noInterrupts();
+    Serial.println("noInterrupts()");
+    for (int i = 0; i < samplesRead; i++) {
+      Serial.println(sampleBuffer[i]);
+    }
+    interrupts();
+    Serial.println("noInterrupts()");
+    samplesRead = 0;
+  }
+}
+
+void onPDMdata() {
+  int bytesAvailable = PDM.available();
+  PDM.read(sampleBuffer, bytesAvailable);
+  samplesRead = bytesAvailable / 2;
+}
